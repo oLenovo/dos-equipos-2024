25dde6758443919862ad0a1109d08b7d80a1f605
oLenovo <arvindswamy2009@gmail.com>
2024-11-12 16:54:24 -0700

    Update DriveTrain.java

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -5,8 +5,8 @@ import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
-import com.revrobotics.CANSparkBase.IdleMode; //idk why these are showing up as errors
-import com.revrobotics.CANSparkLowLevel.MotorType; //idk why this is showing up as an error
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,10 +19,6 @@ public class DriveTrain extends SubsystemBase{
     CCSparkMax frontRight = new CCSparkMax ("front right", "fr", Constants.MotorConstants.FRONT_RIGHT, MotorType.kBrushless, IdleMode.kBrake, Constants.MotorConstants.FRONT_RIGHT_REVERSE);
     CCSparkMax backLeft = new CCSparkMax ("front left", "bl", Constants.MotorConstants.BACK_LEFT, MotorType.kBrushless, IdleMode.kBrake, Constants.MotorConstants.BACK_LEFT_REVERSE);
     CCSparkMax backRight = new CCSparkMax ("front left", "br", Constants.MotorConstants.BACK_RIGHT, MotorType.kBrushless, IdleMode.kBrake, Constants.MotorConstants.BACK_RIGHT_REVERSE);
-    // Spark frontLeft  = new Spark(Constants.MotorConstants.FRONT_LEFT);
-    // Spark frontRight = new Spark(Constants.MotorConstants.FRONT_RIGHT);
-    // Spark backLeft = new Spark (Constants.MotorConstants.BACK_LEFT);
-    // Spark backRight = new Spark (Constants.MotorConstants.BACK_RIGHT);
     DifferentialDrive frontDifferentialDrive = new DifferentialDrive (frontLeft, frontRight);
     DifferentialDrive backDifferentialDrive = new DifferentialDrive(backLeft, backRight);
     public void difDrive (double moveSpeed, double turnSpeed){
@@ -32,7 +28,4 @@ public class DriveTrain extends SubsystemBase{
     public Command drive (double moveSpeed, double turnSpeed){
         this.run (()-> difDrive(moveSpeed, turnSpeed));
     }
-
-
-
 }

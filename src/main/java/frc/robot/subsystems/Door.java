38c75a7ef5b7d36e6a5780c30766e24fde08f819
oLenovo <arvindswamy2009@gmail.com>
2024-11-12 16:59:49 -0700

    Update Door.java

--- src/main/java/frc/robot/subsystems/Door.java
+++ src/main/java/frc/robot/subsystems/Door.java
@@ -1,8 +1,8 @@
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.helpers.CCSparkMax;
 import frc.robot.Constants;
-import com.revrobotics.CANSparkBase.IdleMode; //idk why these are showing up as errors
-import com.revrobotics.CANSparkLowLevel.MotorType; //idk why this is showing up as an error
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Door extends SubsystemBase{
     CCSparkMax door = new CCSparkMax ("door", "d", Constants.MotorConstants.DOOR, MotorType.kBrushless, IdleMode.kBrake, Constants.MotorConstants.DOOR_REVERSE);
